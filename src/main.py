0d497a5f33f840657d8181a4256cffefcd2c484a
ilkinha35-art <ilkinha35@gmail.com>
2026-03-11 14:23:45 -0300

    main.py

---
+++ src/main.py
@@ -0,0 +1,36 @@
+import arcade
+from game_logic import Vehicle
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "London Life - Arcade"
+
+class LondonLife(arcade.Window):
+    def __init__(self):
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+        arcade.set_background_color(arcade.color.SKY_BLUE)
+
+        # Lista de veículos
+        self.vehicles = [
+            Vehicle(100, 200, 80, 40, arcade.color.RED, 3),   # carro
+            Vehicle(300, 300, 120, 50, arcade.color.BLUE, -2), # ônibus
+            Vehicle(500, 400, 80, 40, arcade.color.GREEN, 4), # carro
+        ]
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_text("London Life - Trânsito fluindo", 10, 570, arcade.color.BLACK, 20)
+        # Desenha veículos
+        for v in self.vehicles:
+            v.draw()
+
+    def on_update(self, delta_time):
+        for v in self.vehicles:
+            v.update()
+
+def main():
+    window = LondonLife()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
