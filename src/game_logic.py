53d132805f9dd12b0084bf4d3dd8d7c7db504b69
ilkinha35-art <ilkinha35@gmail.com>
2026-03-11 14:23:15 -0300

    game_logic.py

---
+++ src/game_logic.py
@@ -0,0 +1,22 @@
+import arcade
+import random
+
+class Vehicle:
+    def __init__(self, x, y, width, height, color, speed):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.color = color
+        self.speed = speed
+
+    def draw(self):
+        arcade.draw_rectangle_filled(self.x, self.y, self.width, self.height, self.color)
+
+    def update(self):
+        self.x += self.speed
+        # Se sair da tela, volta do outro lado
+        if self.speed > 0 and self.x > 850:
+            self.x = -50
+        elif self.speed < 0 and self.x < -50:
+            self.x = 850
